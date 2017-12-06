5b98de7e947758d2af64ea9705a4e4ef99980249
Stanislas Brodin <stanislas.brodin@bluekango.com>
2017-12-06 14:50:47 +0100

    Adaptation a l'architecture en sous repertoires

--- admin/create_user.php
+++ admin/create_user.php
@@ -2,14 +2,14 @@
 $dir_name = 'test';
 
 // Création du répertoire
-mkdir('../' . $dir_name);
+mkdir('../user/' . $dir_name);
 
 // Création du .htaccess
 $htaccess_content = "AuthName \"Page d'administration protégée, veuillez vous identifier\"\n";
 $htaccess_content.= "AuthType Basic\n";
 $htaccess_content.= 'AuthUserFile "/var/www/html/dev/counter/' . $dir_name . "/.htpasswd\"\n";
 $htaccess_content.= 'Require valid-user';
-file_put_contents('../' . $dir_name . '/.htaccess', $htaccess_content);
+file_put_contents('../user/' . $dir_name . '/.htaccess', $htaccess_content);
 
 // Création du .htpasswd
 // Password to be encrypted for a .htpasswd file
@@ -18,4 +18,4 @@ $clearTextPassword = 'counter_admin_password';
 $password = crypt($clearTextPassword, base64_encode($clearTextPassword));
 // Print encrypted password
 $htpasswd_content = $dir_name . ':' . $password;
-file_put_contents('../' . $dir_name . '/.htpasswd', $htpasswd_content);
+file_put_contents('../user/' . $dir_name . '/.htpasswd', $htpasswd_content);
