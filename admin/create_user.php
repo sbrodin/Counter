26b37c6fdc2a9e80faf782ef8e903be4706ad85c
Stanislas Brodin <stanislas.brodin@bluekango.com>
2017-12-06 10:56:48 +0100

    Ajout de l'admin

---
+++ admin/create_user.php
@@ -0,0 +1,21 @@
+<?php
+$dir_name = 'test';
+
+// Création du répertoire
+mkdir('../' . $dir_name);
+
+// Création du .htaccess
+$htaccess_content = "AuthName \"Page d'administration protégée, veuillez vous identifier\"\n";
+$htaccess_content.= "AuthType Basic\n";
+$htaccess_content.= 'AuthUserFile "/var/www/html/dev/counter/' . $dir_name . "/.htpasswd\"\n";
+$htaccess_content.= 'Require valid-user';
+file_put_contents('../' . $dir_name . '/.htaccess', $htaccess_content);
+
+// Création du .htpasswd
+// Password to be encrypted for a .htpasswd file
+$clearTextPassword = 'counter_admin_password';
+// Encrypt password
+$password = crypt($clearTextPassword, base64_encode($clearTextPassword));
+// Print encrypted password
+$htpasswd_content = $dir_name . ':' . $password;
+file_put_contents('../' . $dir_name . '/.htpasswd', $htpasswd_content);
