f3f3e0e749fe66a1ff89fc5ec9f244a83e0361d9
Stanislas Brodin <stanislas.brodin@bluekango.com>
2017-12-07 10:47:29 +0100

    Ajout des fichiers de connexion et déconnexion a la base de donnees

---
+++ database_connect.php
@@ -0,0 +1,7 @@
+<?php
+require 'connexion_data.php';
+$mysqli = new mysqli($host, $username, $password, $database);
+if ($mysqli->connect_error) {
+    var_dump(mysqli_connect_error());
+}
+$mysqli->set_charset('utf8');
